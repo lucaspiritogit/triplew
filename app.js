82da5c7cc70a3eaa7a76870275c230125183b691
lucaspiritogit <lucasignaciopirito@gmail.com>
2021-07-18 04:11:11 -0300

    Added basic guide layout and siden navbar

--- app.js
+++ app.js
@@ -7,39 +7,20 @@ app.set('views', __dirname + '/views');
 
 app.use(express.static(__dirname + '/public'));
 
-// app get para responder la solicitud con las views de las paginas
-
-app.get("/", (req, res) => {
-  res.render("index", 
-  {title : " | Inicio"});
-});
-
-app.get("/html", (req, res) => {
-  res.render("html", 
-  {title : " | HTML"});
-});
-
-app.get("/css", (req, res) => {
-  res.render("css", 
-  {title : " | CSS"});
-});
-
-app.get("/javascript", (req, res) => {
-  res.render("javascript", 
-  {title : " | javaScript"});
-});
+// routes
+app.use('/', require('./router/webroutes'));
 
 
 
-
-// /////////////////////////////////
-
-app.listen(port, () => {
-  console.log(`App listening at http://localhost:${port}`);
-});
-
+// /////
 
 app.use((req, res, next) => {
   res.status(404).render("404", 
   {title : " | 404"})
+});
+
+
+
+app.listen(port, () => {
+  console.log(`App listening at http://localhost:${port}`);
 });
---
+++ router/webroutes.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const router = express.Router();
+
+// responder la solicitud con las views de las paginas
+
+
+
+router.get("/", (req, res) => {
+    res.render("index", 
+    {title : " | Inicio"});
+  });
+  
+  router.get("/html", (req, res) => {
+    res.render("html", 
+    {title : " | HTML"});
+  });
+  
+  router.get("/css", (req, res) => {
+    res.render("css", 
+    {title : " | CSS"});
+  });
+  
+  router.get("/javascript", (req, res) => {
+    res.render("javascript", 
+    {title : " | javaScript"});
+  });
+  
+  
+  module.exports = router;
+
+  
+  
+  
+  // /////////////////////////////////
