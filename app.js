20dddefe4559356600b9419ee5783feaaf9382c1
lucaspiritogit <lucasignaciopirito@gmail.com>
2021-07-29 02:03:36 -0300

    heroku3

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 const express = require("express");
 const app = express();
-const port = 3000;
-
-app.set('port', process.env.PORT || port);
+const port = process.env.PORT || 3000;
 
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
