a75862f985598a1027b63f13dcf400b6104931df
Nahuefz <nahuelfontenez@gmail.com>
2021-08-02 03:00:54 -0300

    cambios varios

--- router/webroutes.js
+++ router/webroutes.js
@@ -5,30 +5,30 @@ const router = express.Router();
 // responder la solicitud con las views de las paginas
 
 router.get("/", (req, res) => {
-    res.render("index", 
-    {title : " | Inicio"});
-  });
-  
-  router.get("/html", (req, res) => {
-    res.render("html", 
-    {title : " | HTML"});
-  });
-  
-  router.get("/css", (req, res) => {
-    res.render("css", 
-    {title : " | CSS"});
-  });
-  
-  router.get("/javascript", (req, res) => {
-    res.render("javascript", 
-    {title : " | javaScript"});
-  });
-  
+  res.render("index",
+    { title: " | Inicio" });
+});
 
-  
-  module.exports = router;
+router.get("/html", (req, res) => {
+  res.render("html",
+    { title: " | HTML" });
+});
 
-  
-  
-  
+router.get("/css", (req, res) => {
+  res.render("css",
+    { title: " | CSS" });
+});
+
+router.get("/javascript", (req, res) => {
+  res.render("javascript",
+    { title: " | JavaScript" });
+});
+
+
+
+module.exports = router;
+
+
+
+
   // /////////////////////////////////
